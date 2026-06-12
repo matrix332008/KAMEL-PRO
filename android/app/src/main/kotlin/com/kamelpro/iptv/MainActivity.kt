62e243439de0f4675dbc7e25efec2b0b735ca15f
Kamel Assakri <matrix332008@gmail.com>
2026-06-13 01:39:38 +0200

    Replace getWifiMac with getStableMac for MAC address

--- android/app/src/main/kotlin/com/kamelpro/iptv/MainActivity.kt
+++ android/app/src/main/kotlin/com/kamelpro/iptv/MainActivity.kt
@@ -3,8 +3,8 @@ package com.kamelpro.iptv
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import java.net.NetworkInterface
-import java.util.Collections
+import android.provider.Settings
+import java.security.MessageDigest
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.kamelpro.iptv/mac"
@@ -13,21 +13,19 @@ class MainActivity: FlutterActivity() {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             if (call.method == "getMac") {
-                result.success(getWifiMac())
+                result.success(getStableMac())
             }
         }
     }
 
-    private fun getWifiMac(): String {
-        try {
-            val interfaces = Collections.list(NetworkInterface.getNetworkInterfaces())
-            for (nif in interfaces) {
-                if (nif.name.equals("wlan0", ignoreCase = true)) {
-                    val mac = nif.hardwareAddress ?: continue
-                    return mac.joinToString(":") { String.format("%02X", it) }
-                }
-            }
-        } catch (_: Exception) {}
-        return "02:00:00:00:00:00"
+    private fun getStableMac(): String {
+        return try {
+            val androidId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID) ?: "unknown"
+            val digest = MessageDigest.getInstance("SHA-1").digest(androidId.toByteArray())
+            val hex = digest.joinToString("") { "%02X".format(it) }.substring(0, 12)
+            hex.chunked(2).joinToString(":")
+        } catch (e: Exception) {
+            "02:00:00:00"
+        }
     }
 }
